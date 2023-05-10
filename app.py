da2deed553f1b9fa8a92020bc50067df37a585bc
jtdub <amascuba@gmail.com>
2023-05-09 22:26:40 -0500

    Initial FastAPI app

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import asyncio
 import socket
 
 from fastapi import FastAPI, HTTPException
@@ -13,6 +14,9 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Bind the socket to a particular address and port
 sock.bind(("", 8889))
 
+# Send initial 'command' to Tello drone to enable SDK mode.
+sock.sendto("command".encode(), tello_address)
+
 
 @app.post("/executeCommand/")
 async def execute_command(command: str):
@@ -22,7 +26,12 @@ async def execute_command(command: str):
     try:
         # Send the command to the Tello drone
         sock.sendto(command.encode(), tello_address)
+
+        # Wait for a response (1024 is the buffer size)
+        data, server = sock.recvfrom(1024)
+        response = data.decode(encoding="utf-8").strip()
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-    return {"message": "Command executed successfully"}
+    return {"message": "Command executed successfully", "response": response}
